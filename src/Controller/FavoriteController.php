2d21932c9b0aecb255b6162033b40f0c3df4a096
RiperPro03 <as.riperpro.pf@gmail.com>
2023-11-08 16:42:57 +0100

    Ajout du controller Favorite + page suggestion
    Page /favorite
    Page /suggestion

---
+++ src/Controller/FavoriteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FavoriteController extends AbstractController
+{
+    /**
+     * @Route("/favorite", name="app_favorite")
+     */
+    public function index(): Response
+    {
+        return $this->render('favorite.html.twig', [
+            'controller_name' => 'FavoriteController',
+        ]);
+    }
+
+    /**
+     * @Route("/suggestion", name="app_suggestion")
+     */
+    public function suggestion(): Response
+    {
+        return $this->render('suggestion.html.twig', [
+            'controller_name' => 'FavoriteController',
+        ]);
+    }
+}
