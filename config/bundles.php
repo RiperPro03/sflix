2ab11e17f61123d0e2ba0703b6479d93e2bcf07d
RiperPro03 <as.riperpro.pf@gmail.com>
2024-01-18 17:01:10 +0100

    remove profiler pack

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
